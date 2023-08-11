4088b802050c290e744588fd1cb20232c1e59867
Min Seop Kim <k_ms1998@naver.com>
2023-08-11 12:21:37 +0900

    Level 4: Subproblem을 그대로 합치면 되는 DP

---
+++ code_tree/src/main/java/level4/dp1/Q1/Q1.java
@@ -0,0 +1,22 @@
+package level4.dp1.Q1;
+
+import java.io.*;
+import java.util.*;
+
+public class Q1 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int a1 = 1;
+        int a2 = 1;
+        int answer = 1;
+        for(int i = 2; i < n; i++){
+            answer = a1 + a2;
+            a1 = a2;
+            a2 = answer;
+        }
+
+        System.out.println(answer);
+    }
+}
